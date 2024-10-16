d38bdd323fc564d125439436b619681ec03678b0
RST98\Julius Fiedler <ichderjulius@gmx.de>
2024-10-16 15:28:21 +0200

    manipulator working

--- classical/manipulator_theory.py
+++ classical/manipulator_theory.py
@@ -1,8 +1,9 @@
 from scipy.integrate import solve_bvp, trapezoid
 import numpy as np
-from ipydex import IPS
+from ipydex import IPS, activate_ips_on_exception
 import matplotlib.pyplot as plt
 
+activate_ips_on_exception()
 t0 = 0
 tend = 1.8
 N = 6
@@ -47,10 +48,11 @@ def get_full_a(a1, a2, all=False):
     else:
         return a_rest
 
-def get_v(t, a):
+def get_v(t, p):
     v = 0
     for k in range(N):
-        v += a[k] * theta(t, k)
+        v += p[k] * theta(t, k)
+    # v = p[0] + p[1] * t + p[2] * t**2 + p[3] * t**3 + p[4] * t**4 + p[5] * t**5
     return v
 
 def rhs(t, x, p):
@@ -62,8 +64,10 @@ def rhs(t, x, p):
     # x3... omega1, velocity of first arm
     # x4... omega2, velocity of second arm
 
-    a = get_full_a(p[0], p[1], all=True)
-    v = get_v(t, a)
+    # a = get_full_a(p[0], p[1], all=True)
+    # v = get_v(t, a)
+    v = get_v(t, p)
+
 
     dxdt1 = omega1
     dxdt2 = omega2
@@ -71,9 +75,11 @@ def rhs(t, x, p):
     dxdt4 = -v * (1 + eta*np.cos(phi2)) - eta*omega1**2*np.sin(phi2)
     return np.array([dxdt1, dxdt2, dxdt3, dxdt4])
 
+# def bc(xa, xb, p):
+#     a = get_full_a(p[0], p[1], all=True)
+#     return np.concatenate((xa-b1, xb-b2, np.array([get_v(0, a), get_v(tend, a)])))
 def bc(xa, xb, p):
-    a = get_full_a(p[0], p[1], all=True)
-    return np.concatenate((xa-b1, xb-b2, np.array([get_v(0, a), get_v(tend, a)])))
+    return np.concatenate((xa-b1, xb-b2, np.array([get_v(0, p), get_v(tend, p)])))
 
 
 """
@@ -82,32 +88,38 @@ but only 2 of those 6 are actually relevant, the rest dont matter
 -> first 2 are used to calculate a3-a6
 """
 t = np.linspace(t0, tend, 50)
-x0 = np.zeros((4, t.size))
-x0[:,0] = b1
+x0 = np.ones((4, t.size))
+x0[0] = b1[0]
+x0[1] = b1[1]
+x0[2] = b1[2]
+x0[3] = b1[3]
 
 res = solve_bvp(rhs, bc, t, x0, p=np.ones(N), verbose=2)
 
 
 
 # cartesian coordinates
-l1 = 3
-l2 = 5
+l1 = 0.3
+l2 = 0.5
 x_inner = l1 * np.cos(res.sol(t)[0])
 y_inner = l1 * np.sin(res.sol(t)[0])
 
-x_outer = x_inner + l2 * np.cos(res.sol(t)[1])
-y_outer = y_inner + l2 * np.sin(res.sol(t)[1])
+x_outer = x_inner + l2 * np.cos(res.sol(t)[0] + res.sol(t)[1])
+y_outer = y_inner + l2 * np.sin(res.sol(t)[0] + res.sol(t)[1])
 
-plt.plot(x_inner, y_inner, label="joint 1")
-plt.plot(x_outer, y_outer, label="joint 2", linestyle="dashed")
+plt.scatter(x_inner, y_inner, label="joint 1")
+plt.scatter(x_outer, y_outer, label="joint 2", linestyle="dashed")
 plt.legend()
+plt.gca().set_aspect('equal')
 plt.grid()
 
 
 # plot state components
 fig, ax = plt.subplots(3,1)
-ax[0].plot(t, res.sol(t)[0], label=f"$x_{1}$")
-ax[0].plot(t, res.sol(t)[1], label=f"$x_{2}$", linestyle="dashed")
+ax[0].plot(t, res.sol(t)[0], label=f"$x_{1}$", color="tab:blue")
+ax[0].scatter(t[-1], b2[0], color="tab:blue")
+ax[0].plot(t, res.sol(t)[1], label=f"$x_{2}$", linestyle="dashed", color="tab:orange")
+ax[0].scatter(t[-1], b2[1], color="tab:orange")
 ax[0].legend()
 ax[0].grid()
 
@@ -123,4 +135,48 @@ ax[2].grid()
 
 
 plt.show()
+
+import matplotlib.animation as animation
+fig = plt.figure(figsize=(5, 4))
+ax = fig.add_subplot(autoscale_on=False, xlim=(-1, 1), ylim=(-1, 1.))
+ax.set_aspect('equal')
+ax.grid()
+
+c1 = plt.Circle((0,0), l1, fill=False)
+c2 = plt.Circle((0,l1), l2, fill=False)
+ax.add_patch(c1)
+ax.add_patch(c2)
+
+ax.plot(
+    [0, l1*np.cos(b2[0]), l1*np.cos(b2[0]) + l2*np.cos(b2[0]+b2[1])],
+    [0, l1*np.sin(b2[0]), l1*np.sin(b2[0]) + l2*np.sin(b2[0]+b2[1])],
+    'o-', lw=1, color="red")
+
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], '.-', lw=1, ms=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+dt = 0.01
+
+def animate(i):
+    thisx = [0, x_inner[i], x_outer[i]]
+    thisy = [0, y_inner[i], y_outer[i]]
+
+    history_x = x_outer[:i]
+    history_y = y_outer[:i]
+
+    line.set_data(thisx, thisy)
+    trace.set_data(history_x, history_y)
+    time_text.set_text(time_template % (i*dt))
+
+    c2.set_center((x_inner[i], y_inner[i]))
+    return line, trace, time_text, c2
+
 IPS()
+ans = input("animate?")
+if ans == "y":
+    y = np.empty((len(x_inner), 4))
+    ani = animation.FuncAnimation(
+        fig, animate, len(y), interval=dt*1000, blit=True)
+    gif_writer = animation.ImageMagickWriter(fps=20)
+    ani.save('anim_dip.gif', writer=gif_writer)
